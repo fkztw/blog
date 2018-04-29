cea68a7fa17470001ba79c3e11cfae8ce01c56e3
shunyi <M157q.tw@gmail.com>
2018-04-30 07:41:30 +0800

    [pelicanconf] Change BTC, ETH and LTC addresses

--- pelicanconf.py
+++ pelicanconf.py
@@ -114,9 +114,9 @@ FEED_ALL_RSS = 'feeds/all.feed.rss.xml'
 
 # Donation section for article.html of my forked plumage theme
 DONATIONS = {
-    'btc': '1HWMzBGTQ8VUJGXgvSDzAFacfeY2sLiZda',
-    'eth': '0x0380433c5cdda13c8cd6cff5ddbcc1d7701bcb83',
-    'ltc': 'LUFkjq32kEAiYqnMG25isqGNdgkehj9rJT',
+    'btc': '3PpJHALguhGcA3An3GDyQb6x7yDwm9K38B',
+    'eth': '0x0c77e7200179801e8f191975683322725abe234c',
+    'ltc': 'LXrgT5JMW6vrua7BbkhHbeyip1HnXH6WKg',
     'paypalme': 'm157q',
     'opay-broadcaster-donate': '4E163AAE10E448A30DB244CF85527271',
 }
